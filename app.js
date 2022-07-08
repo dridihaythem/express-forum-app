b64eac5aba456b17205788ea4e336dc9beecc97a
dridihaythem <haithemdridiweb@gmail.com>
2022-07-08 18:11:21 +0100

    use global error middleware

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const errorMiddleware = require('./utils/errorMiddleware');
 
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
+
+app.use(errorMiddleware);
 
 module.exports = app;
