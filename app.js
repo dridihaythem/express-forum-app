a76b2d23b85ee003bdf258e4059dceb00227791b
dridihaythem <haithemdridiweb@gmail.com>
2022-07-08 18:11:36 +0100

    create 404 page

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const AppError = require('./utils/AppError');
 const errorMiddleware = require('./utils/errorMiddleware');
 
 const app = express();
 
 app.use(express.json());
+
+app.all('*', (req, res, next) => {
+	next(new AppError('endpoint not found', 404));
+});
 
 app.use(errorMiddleware);
 
