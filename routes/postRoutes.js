c06a2034bc1b818330e757a600bc92599a18c1f3
dridihaythem <haithemdridiweb@gmail.com>
2022-07-12 00:40:41 +0100

    prevent banned users from creating new post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { createPost, getPost, getAllPosts } = require('../controllers/postController');
-const { auth, restrictTo } = require('../middlewares/authMiddleware');
+const { auth, restrictTo, notBanned } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.route('/').get(getAllPosts).post(auth, restrictTo('user'), createPost);
+router.route('/').get(getAllPosts).post(auth, notBanned, createPost);
 router.route('/:id').get(getPost);
 
 module.exports = router;
