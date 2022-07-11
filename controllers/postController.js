cbc75d732eef12777bcd40ebf42e88603dd2ff68
dridihaythem <haithemdridiweb@gmail.com>
2022-07-12 00:05:55 +0100

    create new post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 const Post = require('../models/postModel');
+const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.createPost = catchAsync(async (req, res, next) => {
@@ -13,5 +14,19 @@ exports.createPost = catchAsync(async (req, res, next) => {
 	res.status(201).json({
 		status: 'success',
 		data: { _id: post._id, title: post.title, content: post.content },
+	});
+});
+
+exports.getPost = catchAsync(async (req, res, next) => {
+	const post = await Post.findOne({ _id: req.params.id, published: true })
+		.select('slug title content')
+		.populate({ path: 'userId', select: 'first_name last_name' });
+
+	if (!post) {
+		return next(new AppError('Post not found', 404));
+	}
+	res.status(200).json({
+		status: 'success',
+		data: post,
 	});
 });
