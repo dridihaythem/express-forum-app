fb92cce0d257cbe4928c35ae89174ba19574044b
dridihaythem <haithemdridiweb@gmail.com>
2022-07-12 01:53:36 +0100

    [improve] post Middleware

--- middlewares/postMiddleware.js
+++ middlewares/postMiddleware.js
@@ -4,6 +4,15 @@ const AppError = require('../utils/AppError');
 // check if user is admin or moderator
 // or user is the author of this post
 exports.canUpdateOrDeletePost = async (req, res, next) => {
+	// user can update or delete only published posts
+	const filter = { _id: req.params.id, published: true };
+	// admin or moderator can update or delete any posts
+	if (['admin', 'moderator'].includes(req.user.role)) {
+		delete filter.published;
+	}
+
+	console.log(filter);
+
 	const post = await Post.findById(req.params.id);
 
 	if (!post) {
