9df704723a033578f4a1e79d816bae1e57e286e6
dridihaythem <haithemdridiweb@gmail.com>
2022-07-16 14:48:39 +0100

    signupRequest validate

---
+++ requests/auth/signupRequest.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+const User = require('../../models/userModel');
+
+const checkUsedEmail = async (value, helpers) => {
+	const check = await User.findOne({ email: value });
+	if (check) {
+		throw new Error('email already exists');
+	}
+	return value;
+};
+
+const date = new Date();
+date.setFullYear(date.getFullYear() - 13);
+
+const signupRequest = Joi.object({
+	first_name: Joi.string().trim().min(3).max(10).required().lowercase(),
+	last_name: Joi.string().trim().min(3).max(10).required().lowercase(),
+	birthday: Joi.date().less(date).required().messages({
+		'date.less': 'You must be at least 13 years old',
+	}),
+	email: Joi.string().email().required().lowercase().external(checkUsedEmail),
+	password: Joi.required(),
+	passwordConfirmation: Joi.string().required().valid(Joi.ref('password')),
+});
+
+module.exports = signupRequest;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const loginRequest = require('../requests/auth/loginRequest');
+const signupRequest = require('../requests/auth/signupRequest');
 const validate = require('../utils/validate');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
+router.post('/signup', validate(signupRequest), authController.signup);
 router.post('/login', validate(loginRequest), authController.login);
 router.post('/forget-password', authController.forgetPassword);
 router.patch('/reset-password/:token', authController.resetPassword);
