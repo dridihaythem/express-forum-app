722608329e369f64e768620f5f6c7dd375199fd6
dridihaythem <haithemdridiweb@gmail.com>
2022-07-16 14:28:18 +0100

    handle db connection error

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config({path : './.env'})
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-const db = process.env.DATABASE
+const db = process.env.DATABASE;
 
-mongoose.connect(db, { useNewUrlParser: true }).then(con => console.log('DB connected'))
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then((con) => console.log('DB connected'))
+	.catch(() => {
+		console.log("Can't connect to db");
+		process.exit(1);
+	});
 
-app.listen(port,()=>console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
