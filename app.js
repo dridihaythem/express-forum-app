6da6adc138edcf8626afce0b300bcbc6bfc2d0e8
dridihaythem <haithemdridiweb@gmail.com>
2022-07-17 01:03:49 +0100

    add security dependencies

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const compression = require('compression');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const hpp = require('hpp');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const AppError = require('./utils/AppError');
@@ -9,7 +13,11 @@ const postRoutes = require('./routes/postRoutes');
 const swaggerDocument = YAML.load('./swagger.yaml');
 const app = express();
 
-app.use(express.json());
+app.use(helmet());
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize());
+app.use(hpp());
+app.use(compression());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/auth', authRoutes);
