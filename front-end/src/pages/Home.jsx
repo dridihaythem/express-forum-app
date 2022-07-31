08869914348fe92ff909b2c26744a47296b005ac
dridihaythem <haithemdridiweb@gmail.com>
2022-07-31 03:37:09 +0100

    Init home page

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export default function Home() {
+	return (
+		<>
+			<Helmet>
+				<title>MERN FORUM</title>
+			</Helmet>
+			<div>welcome</div>
+		</>
+	);
+}
