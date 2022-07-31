c46cb8c0cae6e4616aa8931236acc5242e0de0a2
dridihaythem <haithemdridiweb@gmail.com>
2022-07-31 03:36:30 +0100

    create loading component

---
+++ front-end/src/components/UI/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loading() {
+	return (
+		<div className='spinner-border ms-2' role='status'>
+			<span className='visually-hidden'>Loading...</span>
+		</div>
+	);
+}
