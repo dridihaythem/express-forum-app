cac7292006b18999a3e904a148971ec066997224
dridihaythem <haithemdridiweb@gmail.com>
2022-07-31 20:11:40 +0100

    Navbar show login user data

--- front-end/src/components/Layout/Navbar.jsx
+++ front-end/src/components/Layout/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import Logo from '../../assets/images/logo.png';
+import { authActions } from '../../store/auth';
 import classes from './Navbar.module.css';
 
 export default React.forwardRef(function (props, ref) {
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const { auth, user } = useSelector((state) => state.auth);
+
+	const logoutHandler = () => {
+		dispatch(authActions.logout());
+		navigate('/auth/login');
+	};
 	return (
 		<nav className={`navbar navbar-expand-lg bg-dark  navbar-dark mb-2 ${classes.navbar}`}>
 			<div className='container'>
@@ -31,16 +42,51 @@ export default React.forwardRef(function (props, ref) {
 						</li>
 					</ul>
 					<ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
-						<li className={`nav-link ${classes.link}`}>
-							<Link className='nav-link' to='/auth/login'>
-								<i className='fa-solid fa-arrow-right-to-bracket'></i> Login
-							</Link>
-						</li>
-						<li className={`nav-link ${classes.link}`}>
-							<Link className='nav-link' to='/auth/register'>
-								<i className='fa-solid fa-user-plus'></i> Create new Account
-							</Link>
-						</li>
+						{!auth && (
+							<>
+								<li className={`nav-link ${classes.link}`}>
+									<Link className='nav-link' to='/auth/login'>
+										<i className='fa-solid fa-arrow-right-to-bracket'></i> Login
+									</Link>
+								</li>
+								<li className={`nav-link ${classes.link}`}>
+									<Link className='nav-link' to='/auth/register'>
+										<i className='fa-solid fa-user-plus'></i> Create new Account
+									</Link>
+								</li>
+							</>
+						)}
+						{auth && (
+							<>
+								<li className={`nav-link dropdown`}>
+									<a
+										className='nav-link dropdown-toggle'
+										href='#'
+										role='button'
+										data-bs-toggle='dropdown'
+										aria-expanded='false'
+									>
+										<img className={`rounded-circle ${classes['user-avatar']}`} src={user.photo} />
+										{user.first_name} {user.last_name}
+									</a>
+									<ul className='dropdown-menu'>
+										<li>
+											<Link to='/profile' className='dropdown-item'>
+												<i className='fa-solid fa-pen'></i> Edit my profile
+											</Link>
+										</li>
+										<li>
+											<hr className='dropdown-divider' />
+										</li>
+										<li>
+											<a onClick={logoutHandler} className='dropdown-item cursor-pointer'>
+												<i className='fas fa-sign-out'></i> Logout
+											</a>
+										</li>
+									</ul>
+								</li>
+							</>
+						)}
 					</ul>
 				</div>
 			</div>
