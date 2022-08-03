18fdc48e1030401c7a64101a84249db852abb68a
dridihaythem <haithemdridiweb@gmail.com>
2022-08-03 19:14:34 +0100

    show "Create new post" link for auth users only

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+	const auth = useSelector((state) => state.auth.auth);
+
 	return (
 		<>
 			<Helmet>
 				<title>MERN FORUM</title>
 			</Helmet>
 			<div>
-				welcome
-				<Link to='/posts/create' className='btn btn-danger'>
-					Create new post
-				</Link>
+				{auth && (
+					<Link to='/posts/create' className='btn btn-danger'>
+						Create new post
+					</Link>
+				)}
 			</div>
 		</>
 	);
