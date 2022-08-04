24523f0580663c3f61f9963119b6ec33f81dc01b
dridihaythem <haithemdridiweb@gmail.com>
2022-08-04 01:44:56 +0100

    get user posts

--- front-end/src/pages/User.jsx
+++ front-end/src/pages/User.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
 import { useParams } from 'react-router-dom';
 import RowCardLayout from '../components/Layout/RowCardLayout';
+import Post from '../components/posts/Post';
 import Loading from '../components/UI/Loading';
 import UserDetails from '../components/users/UserDetails';
 import useHttp from '../hooks/useHttp';
@@ -10,21 +12,46 @@ export default function User() {
 
 	const { id } = params;
 
-	const { loading, state, sendRequest } = useHttp(null, true);
+	const { loading, sendRequest } = useHttp(null, true);
+
+	const [user, setUser] = useState(null);
+
+	const { postsLoading, sendRequest: sendGetPostsRequest } = useHttp(null, true);
+
+	const [posts, setPosts] = useState([]);
 
 	useEffect(() => {
-		sendRequest(`/users/${id}`, 'GET');
+		sendRequest(`/users/${id}`, 'GET', null, (data) => {
+			setUser(data.data);
+		});
+		sendGetPostsRequest(`/posts/findByUser/${id}`, 'GET', null, (data) => {
+			setPosts(data.data);
+		});
 	}, [id]);
 
 	return (
 		<>
+			<Helmet>
+				<title>{!loading && `${user.first_name} ${user.last_name}`}</title>
+			</Helmet>
 			<RowCardLayout
-				title={`${state?.data?.first_name} ${state?.data?.last_name} Profile`}
+				title={!loading && `${user.first_name} ${user.last_name} Profile`}
 				rowClasses={'justify-content-center'}
-				colClasses={'col-md-10 mb-3'}
+				colClasses={'col-md-12 mb-3'}
 			>
-				{loading ? <Loading /> : <UserDetails user={state.data} />}
+				{loading ? <Loading /> : <UserDetails user={user} />}
 			</RowCardLayout>
+
+			{postsLoading ? (
+				<Loading />
+			) : (
+				<>
+					<h3 className='my-3'>Posts : </h3>
+					{posts.map((post) => (
+						<Post key={post._id} {...post} user={user} />
+					))}
+				</>
+			)}
 		</>
 	);
 }
